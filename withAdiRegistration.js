c8b8e50525108749deb3cd8406635ee9ce551fec
Al-AmeenMD <alaminmuhammad611@gmail.com>
2026-04-21 12:13:43 +0100

    Initial project setup and Google Play verification

---
+++ withAdiRegistration.js
@@ -0,0 +1,15 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function withAdiRegistration(config) {
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const targetPath = path.join(config.modRequest.platformProjectRoot, 'app', 'src', 'main', 'assets');
+      fs.mkdirSync(targetPath, { recursive: true });
+      fs.writeFileSync(path.join(targetPath, 'adi-registration.properties'), 'CGK6G5NCXTTNWAAAAAAAAAAA');
+      return config;
+    },
+  ]);
+};
